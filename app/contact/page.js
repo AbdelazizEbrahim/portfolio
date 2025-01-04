d7713fa84635dcbe0fcb4cd4a8a593e877cead17
Abdelaziz Ebrahim <abdelazizebrahim5980@gmail.com>
2025-01-04 19:03:13 +0300

    Update

--- app/contact/page.js
+++ app/contact/page.js
@@ -89,10 +89,10 @@ const Contact = () => {
     <motion.section
       initial={{ opacity: 0 }}
       animate={{ opacity: 1, transition: { delay: 0.4, duration: 0.4, ease: 'easeIn' } }}
-      className="py-6"
+      className="py-6 overflow-x-hidden overflow-y-auto"
     >
       <div className="container mx-auto">
-        <div className="flex flex-col xl:flex-row gap-[30px]">
+        <div className="flex flex-col xl:flex-row gap-[px]">
           <div className="xl:w-[54%] order-2 xl:order-none">
             <form
               className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl"
