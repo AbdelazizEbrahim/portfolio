71acd4ac778b89ee92b9f6c51824f7fe20636d13
Abdelaziz Ebrahim <abdelazizebrahim5980@gmail.com>
2025-06-27 10:09:20 +0300

    Update

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -1,26 +1,33 @@
-import React from 'react'
-import Link from 'next/link';
-import {FaGithub, FaLinkedin, FaTiktok, FaTelegram } from 'react-icons/fa';
+import React from "react";
+import Link from "next/link";
+import { FaGithub, FaLinkedin, FaTiktok, FaTelegram } from "react-icons/fa";
 
 const socials = [
-    {icon: <FaGithub/>, path: "https://github.com/abdelazizEbrahim"},
-    {icon: <FaLinkedin/>, path: "https://www.linkedin.com/in/abdelazizEbrahim/"},
-    {icon: <FaTiktok/>, path: "https://www.tiktok.com/@ab_aziz996"},
-    {icon: <FaTelegram/>, path: "https://t.me/abdelazizEbrahim"},
+  { icon: <FaGithub />, path: "https://github.com/abdelazizEbrahim" },
+  {
+    icon: <FaLinkedin />,
+    path: "https://www.linkedin.com/in/abdelaziz-ebrahim1/",
+  },
+  { icon: <FaTiktok />, path: "https://www.tiktok.com/@ab_aziz996" },
+  { icon: <FaTelegram />, path: "https://t.me/abdelazizEbrahim" },
 ];
 
-const Socials = ({containerStyles, iconStyles}) => {
+const Socials = ({ containerStyles, iconStyles }) => {
   return (
     <div className={containerStyles}>
       {socials.map((item, index) => {
         return (
-            <Link target='_blank' key={index} href={item.path} className={iconStyles}>
-                {item.icon}
-            </Link>
-        )
+          <Link
+            target="_blank"
+            key={index}
+            href={item.path}
+            className={iconStyles}>
+            {item.icon}
+          </Link>
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
-export default Socials
+export default Socials;
